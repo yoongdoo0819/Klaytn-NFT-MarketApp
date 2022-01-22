ca9f59b42a66a8a5bd97afd7368d20d6f613e8cb
YoongdooNoh <yoongdoo0819@naver.com>
2022-01-23 06:26:08 +0900

    caver-js 첫 적용, webpack5 이슈 해결(webpack@4.44.2, react-scripts@4.0.3)

--- klay-market/src/App.js
+++ klay-market/src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Caver from 'caver-js'; // webpack5 이슈 발생 시, webpack@4.44.2 / react-scripts@4.0.3로 다운그레이드 적용하면 해결 
+ 
+//  const CONTRACT_ADDRESS = '';
+//  const ACCESS_KEY_ID = '';
+//  const SECRET_ACCESS_KEY = '';
+const CHAIN_ID = '1001'; // MainNet : 8217 , TestNet : 1001
+const option = {
+  headers : [
+      {
+        name: "Authorization",
+        value: "Basic " + 'S0FTS0NSUEY0RFM1SzRMWExNSjgzSDdBOlB6Njg5dFVIWUhGc0JGMHVWRmQtcllnclZZR2NTMzRwdTgyQVdvRno='
+      },
+      {name: "x-chain-id", value: CHAIN_ID}
+  ]
+}
+
+const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
+const getBalance = (address) => {
+  
+  return caver.rpc.klay.getBalance(address).then((response) => {
+    const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(response));
+    console.log(balance);
+    return balance;
+  })
+}
 
 function App() {
+  getBalance('0x860ab241a263ee4445d6ee2cb6cc5c9d2d5cbcff');
+
   return (
     <div className="App">
       <header className="App-header">

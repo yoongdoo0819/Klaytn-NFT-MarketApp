3f34aea9df8a4a729242636d9dd886bd370911e3
YoongdooNoh <yoongdoo0819@naver.com>
2022-01-24 10:06:34 +0900

    readNumber() from Storage Contract

--- klay-market/src/App.js
+++ klay-market/src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Caver from 'caver-js'; // webpack5 이슈 발생 시, webpack@4.44.2 / react-scripts@4.0.3로 다운그레이드 적용하면 해결 
- 
-//  const CONTRACT_ADDRESS = '';
-//  const ACCESS_KEY_ID = '';
-//  const SECRET_ACCESS_KEY = '';
+
+
+const STORAGE_CONTRACT_ADDRESS = '0x84F3AE708621c706ba5D273DeE679b92376631c6';  // 테스트넷에 배포된 컨트랙트 (Storage.sol) 주소
+const STORAGE_ABI = '[ { "inputs": [], "name": "retrieve", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "num", "type": "uint256" } ], "name": "store", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]'
+
+// 다음은 Klaytn API Service 사용을 위해 발급 받은 인증 정보
+const ACCESS_KEY_ID = '';
+const SECRET_ACCESS_KEY = '';
 const CHAIN_ID = '1001'; // MainNet : 8217 , TestNet : 1001
 const option = {
   headers : [
       {
         name: "Authorization",
-        value: "Basic " + 'S0FTS0NSUEY0RFM1SzRMWExNSjgzSDdBOlB6Njg5dFVIWUhGc0JGMHVWRmQtcllnclZZR2NTMzRwdTgyQVdvRno='
+        value: "Basic " + 'S0FTS0NSUEY0RFM1SzRMWExNSjgzSDdBOlB6Njg5dFVIWUhGc0JGMHVWRmQtcllnclZZR2NTMzRwdTgyQVdvRno='  // "Basic " + Buffer.from(ACCESS_KEY_ID + ":" + SECRET_ACCESS_KEY).toString("base64") 와 동일
       },
       {name: "x-chain-id", value: CHAIN_ID}
   ]
 }
 
 const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", option));
+const StorageContract = new caver.contract(JSON.parse(STORAGE_ABI), STORAGE_CONTRACT_ADDRESS);
+
+const readNumber = async () => {
+  const num = await StorageContract.methods.retrieve().call();
+  console.log(num)
+}
+
 const getBalance = (address) => {
   
   return caver.rpc.klay.getBalance(address).then((response) => {
@@ -27,6 +38,7 @@ const getBalance = (address) => {
 }
 
 function App() {
+  readNumber();
   getBalance('0x860ab241a263ee4445d6ee2cb6cc5c9d2d5cbcff');
 
   return (
